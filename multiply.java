af138cc88bc6f72920eba111ac07b1fe72c9fd68
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-02-25 20:02:38 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,11 @@
+class Multiply
+{
+    public static void main(String args[])
+    {
+        int num1, num2, multi;
+        num1 = 8;
+        num2 = 9;
+        multi = num1 * num2;
+        System.out.println(multi);
+    }
+}
